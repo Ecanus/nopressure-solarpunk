7b50d507cd4faa848835a7a02cdf1cc8d9372232
tangi-ux <tangi.earls@gmail.com>
2025-05-08 17:38:28 -0700

    fixed start_menu script

--- start_menu_folder/start_menu.gd
+++ start_menu_folder/start_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://scenes/world/world.tscn")
 
 
 func _on_exit_pressed():
